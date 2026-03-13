be3db8cbf82c95eff7e68755935a1cb3fde0e872
Q Z <spellingbeeq@gmail.com>
2026-03-13 13:19:31 -0500

    nt v0.1.6 - switch default extension

--- Formula/nt.rb
+++ Formula/nt.rb
@@ -1,8 +1,8 @@
 class Nt < Formula
   desc "Simple indexed notes maintainer for the command line"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/nt-v0.1.5.tar.gz"
-  sha256 "a8a67813b7d1910be74343f60722504db6a55c3db86fc867dbab601578f0cfa4"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/nt-v0.1.6.tar.gz"
+  sha256 "1a1f12dd762c0d24e2fe46526701bde2ddf9ac6bc85abd70f0322e81adb16190"
   license "MIT"
 
   depends_on "zsh" => :optional
