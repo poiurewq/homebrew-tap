30ba7a21d62199c841f454e034a50e3ad8634c4d
Q Z <spellingbeeq@gmail.com>
2026-03-20 11:32:46 -0400

    clk v0.3.0 - revamp architecture and many qols

--- Formula/clk.rb
+++ Formula/clk.rb
@@ -1,8 +1,8 @@
 class Clk < Formula
   desc "Clock in & out of work, tracking minutes spent per day"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.2.5.tar.gz"
-  sha256 "3289644346e4dcf36bc5eec871f3adf483f709d7f28d47f9d4e8489d1f22faef"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.3.0.tar.gz"
+  sha256 "28e503f7c809f5bdcb1131f0ce583b7495e9b3ddfbe25735dd818588796be205"
   license "MIT"
 
   depends_on "bash"
