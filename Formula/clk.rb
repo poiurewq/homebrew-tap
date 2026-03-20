f0073f54f9ab268858f6a38d7fca0a633527f86a
Q Z <spellingbeeq@gmail.com>
2026-03-20 17:15:37 -0400

    clk v0.3.1 - add qols bar chart and summary stats

--- Formula/clk.rb
+++ Formula/clk.rb
@@ -1,8 +1,8 @@
 class Clk < Formula
   desc "Clock in & out of work, tracking minutes spent per day"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.3.0.tar.gz"
-  sha256 "28e503f7c809f5bdcb1131f0ce583b7495e9b3ddfbe25735dd818588796be205"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.3.1.tar.gz"
+  sha256 "4940a90cc2227fde502553399a24302e1d9b70bfdeec06f799c9fe33688d2cdd"
   license "MIT"
 
   depends_on "bash"
