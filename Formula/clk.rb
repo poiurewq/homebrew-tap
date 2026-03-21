5955a1a34edf7c99f9a70c73e999152c37610c3d
Q Z <spellingbeeq@gmail.com>
2026-03-21 00:14:53 -0400

    clk v0.3.2 - add multitag filter and more aggregations

--- Formula/clk.rb
+++ Formula/clk.rb
@@ -1,8 +1,8 @@
 class Clk < Formula
   desc "Clock in & out of work, tracking minutes spent per day"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.3.1.tar.gz"
-  sha256 "4940a90cc2227fde502553399a24302e1d9b70bfdeec06f799c9fe33688d2cdd"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/clk-v0.3.2.tar.gz"
+  sha256 "06487fa165e91936e26b0b0f661259f86504b44b7cf7992beb8b4d68a603543a"
   license "MIT"
 
   depends_on "bash"
