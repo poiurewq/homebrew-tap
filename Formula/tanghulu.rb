e24131638380478c2becabfffde6165acb07f830
Q Z <spellingbeeq@gmail.com>
2026-04-22 22:06:04 -0400

    tanghulu v0.1.2 - add block preview

--- Formula/tanghulu.rb
+++ Formula/tanghulu.rb
@@ -1,8 +1,8 @@
 class Tanghulu < Formula
   desc "Run a string of timed focus/rest sessions with pop-up reminders"
   homepage "https://github.com/poiurewq/scripts"
-  url "https://github.com/poiurewq/scripts/archive/refs/tags/tanghulu-v0.1.1.tar.gz"
-  sha256 "4584696be07726ca5a85b2da1ba3492ef634f91ffb8c298e584ca380bbdcd448"
+  url "https://github.com/poiurewq/scripts/archive/refs/tags/tanghulu-v0.1.2.tar.gz"
+  sha256 "703732c25d0596e45f2cc14d74aea81c1cf93b2d1d25dd5f49f225d70db21717"
   license "MIT"
 
   depends_on "bash"
